bc172097896c4171f76d19acb5c14f9e11557029
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-06-26 17:18:00 +0300

    working dice

--- script3.js
+++ script3.js
@@ -1,10 +1,23 @@
 "use strict";
 
+const displayPlayerScore = document.querySelector(".player-score");
+const play = document.querySelector(".play-btn");
+displayPlayerScore.textContent = "0";
+
 // Define a player object
 let player = {
   playerName: "",
   playerScore: 0,
 };
+
+//call method
+// const player1 = {
+//   winnerstring: function () {
+//     return `Hei, ${this.name} ,you gained a total of ${this.playerScore}`;
+//   },
+// };
+
+//console.log(player1.winnerstring.call(player));
 
 // Function to roll two dice
 function rollDice() {
@@ -15,8 +28,27 @@ function rollDice() {
 
   const randomNumber = dice1 + dice2;
   console.log(randomNumber);
-  return randomNumber;
+  // return randomNumber;
+
+  if (randomNumber == 7 || randomNumber == 11) {
+    player.playerScore = player.playerScore + 1;
+    displayPlayerScore.textContent = player.playerScore;
+  } else if (randomNumber === 2 || randomNumber == 3 || randomNumber == 12) {
+    player.playerScore--;
+    displayPlayerScore.textContent = player.playerScore;
+  } else return;
+
+  console.log("total points");
+  console.log(player.playerScore);
 }
+
+/////////////////////////////////////////
+//working btn
+play.addEventListener("click", () => {
+  rollDice();
+});
+
+/////////////////////////////////////////////////////////////////////////////
 
 // Function to narrate the story
 function narrate(outcome) {
@@ -39,5 +71,3 @@ let delayedNarrate = delayNarration(narrate, 2000);
 ////////////////////////////////////////////////////////////////////
 //////////////////////implememntation////////////////////////////
 ///////////////////////////////////////////////////////////////////
-
-rollDice();
